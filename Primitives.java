4b6e8a3f4bd7e46418546951728d073cd0aa7534
Aditya Don <adibhai@gmail.com>
2025-08-10 14:10:38 +0530

    commiting primitives file

---
+++ Primitives.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Primitives {
+    public static void main(String[] args) {
+        // Scanner input = new Scanner(System.in);
+        // int rollNum = input.nextInt();
+        // System.out.println("Your rollNum is: " + rollNum);
+
+        long forlongInteger = 2343423423423423L;
+        System.out.println(forlongInteger);
+
+        char c = 'd';
+        System.out.println(c);
+
+        float f = 23.3f;
+        System.out.println(f);
+
+        double forlongFloat = 23234234234532332.4;
+        System.out.println(forlongFloat);
+
+        long largeInteger = 23432434234234245L;
+        System.out.println(largeInteger);
+
+        boolean bool1 = true;
+        System.out.println(bool1);
+
+        boolean bool2 = false;
+        System.out.println(bool2);
+    }
+}
